c7f0ba2fe5fa3db98b20b4bca42245b5c61765a0
codytheroux96 <cody.theroux3@gmail.com>
2024-06-07 20:37:56 -0400

    adding form validation and having the html act accordingly

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,97 +1,112 @@
 package main
 
 import (
-    "errors"
-    "fmt"
-    "net/http"
-    "strconv"
-    "strings"
-    "unicode/utf8"
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"unicode/utf8"
 
-    "snippetbox.cody.net/internal/models"
+	"snippetbox.cody.net/internal/models"
 )
 
+type snippetCreateForm struct {
+	Title       string
+	Content     string
+	Expires     int
+	FieldErrors map[string]string
+}
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-    snippets, err := app.snippets.Latest()
-    if err != nil {
-        app.serverError(w, r, err)
-        return
-    }
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
 
-    data := app.newTemplateData(r)
-    data.Snippets = snippets
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
 
-    app.render(w, r, http.StatusOK, "home.tmpl", data)
+	app.render(w, r, http.StatusOK, "home.tmpl", data)
 
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-    id, err := strconv.Atoi(r.PathValue("id"))
-    if err != nil || id < 1 {
-        http.NotFound(w, r)
-        return
-    }
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
 
-    snippet, err := app.snippets.Get(id)
-    if err != nil {
-        if errors.Is(err, models.ErrNoRecord) {
-            http.NotFound(w, r)
-        } else {
-            app.serverError(w, r, err)
-        }
-        return
-    }
-    
-    data := app.newTemplateData(r)
-    data.Snippet = snippet
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
 
-    app.render(w, r, http.StatusOK, "view.tmpl", data)
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
+
+	app.render(w, r, http.StatusOK, "view.tmpl", data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-    data := app.newTemplateData(r)
+	data := app.newTemplateData(r)
 
-    app.render(w, r, http.StatusOK, "create.tmpl", data)
+	app.render(w, r, http.StatusOK, "create.tmpl", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-    err := r.ParseForm()
-    if err != nil {
-        app.clientError(w, http.StatusBadRequest)
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+    form := snippetCreateForm {
+        Title: r.PostForm.Get("title"),
+        Content: r.PostForm.Get("content"),
+        Expires: expires,
+        FieldErrors: map[string]string{},
+    }
+
+	if strings.TrimSpace(form.Title) == "" {
+		form.FieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(form.Title) > 100 {
+		form.FieldErrors["title"] = "This filed cannot be more than 100 characters long"
+	}
+
+	if strings.TrimSpace(form.Content) == "" {
+		form.FieldErrors["content"] = "This field cannot be blank"
+	}
+
+    if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
+        form.FieldErrors["expires"] = "This field must equal 1, 7, or 365"
+    }
+
+	if len(form.FieldErrors) > 0 {
+		data := app.newTemplateData(r)
+        data.Form = form
+        app.render(w, r, http.StatusUnprocessableEntity, "create.tmpl", data)
         return
-    }
+	}
 
-    title := r.PostForm.Get("title")
-    content := r.PostForm.Get("content")
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
 
-    expires, err := strconv.Atoi(r.PostForm.Get("expires"))
-    if err != nil {
-        app.clientError(w, http.StatusBadRequest)
-        return
-    }
-
-    fieldErrors := make(map[string]string)
-
-    if strings.TrimSpace(title) == "" {
-        fieldErrors["title"] = "This field cannot be blank"
-    } else if utf8.RuneCountInString(title) > 100 {
-        fieldErrors["title"] = "This filed cannot be more than 100 characters long"
-    }
-
-    if strings.TrimSpace(content) == "" {
-        fieldErrors["content"] = "This field cannot be blank"
-    }
-
-    if len(fieldErrors) > 0 {
-        fmt.Fprint(w, fieldErrors)
-        return
-    }
-    
-    id, err := app.snippets.Insert(title, content, expires)
-    if err != nil {
-        app.serverError(w, r, err)
-        return
-    }
-
-    http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
