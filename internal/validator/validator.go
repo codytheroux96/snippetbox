6ffab20268a50969fd79c9563264cd1f6bfdab1c
codytheroux96 <cody.theroux3@gmail.com>
2024-06-08 13:00:20 -0400

    adding validator helper functions

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -14,3 +14,30 @@ func (v *Validator) Valid() bool {
 	return len(v.FieldErrors) == 0
 }
 
+func (v *Validator) AddFieldError(key, message string) {
+	if v.FieldErrors == nil {
+		v.FieldErrors = make(map[string]string)
+	}
+
+	if _, exists := v.FieldErrors[key]; !exists {
+		v.FieldErrors[key] = message
+	}
+}
+
+func (v *Validator) CheckField(ok bool, key, message string) {
+	if !ok {
+		v.AddFieldError(key, message)
+	}
+}
+
+func NotBlank(value string) bool {
+	return strings.TrimSpace(value) != ""
+}
+
+func MaxChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) <= n
+}
+
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
+	return slices.Contains(permittedValues, value)
+}
