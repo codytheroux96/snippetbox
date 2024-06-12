0fab290a63867efc5c75dfe7428e73e4b921c5db
codytheroux96 <cody.theroux3@gmail.com>
2024-06-12 18:53:13 -0400

    updating the userSignup handler as well as the html to display a signup form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -17,6 +17,13 @@ type snippetCreateForm struct {
 	validator.Validator `form:"-"`
 }
 
+type userSignupForm struct {
+	Name                string `form:"name"`
+	Email               string `form:"email"`
+	Password            string `form:"password"`
+	validator.Validator `form:"-"`
+}
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	snippets, err := app.snippets.Latest()
 	if err != nil {
@@ -32,26 +39,26 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-    id, err := strconv.Atoi(r.PathValue("id"))
-    if err != nil || id < 1 {
-        http.NotFound(w, r)
-        return
-    }
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
 
-    snippet, err := app.snippets.Get(id)
-    if err != nil {
-        if errors.Is(err, models.ErrNoRecord) {
-            http.NotFound(w, r)
-        } else {
-            app.serverError(w, r, err)
-        }
-        return
-    }
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
 
-    data := app.newTemplateData(r)
-    data.Snippet = snippet
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
 
-    app.render(w, r, http.StatusOK, "view.tmpl", data)
+	app.render(w, r, http.StatusOK, "view.tmpl", data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -65,53 +72,55 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-    var form snippetCreateForm
+	var form snippetCreateForm
 
-    err := app.decodePostForm(r, &form)
-    if err != nil {
-        app.clientError(w, http.StatusBadRequest)
-        return
-    }
+	err := app.decodePostForm(r, &form)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
-    form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
-    form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
-    form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
-    form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+	form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
 
-    if !form.Valid() {
-        data := app.newTemplateData(r)
-        data.Form = form
-        app.render(w, r, http.StatusUnprocessableEntity, "create.tmpl", data)
-        return
-    }
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusUnprocessableEntity, "create.tmpl", data)
+		return
+	}
 
-    id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
-    if err != nil {
-        app.serverError(w, r, err)
-        return
-    }
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
 
-    app.sessionManager.Put(r.Context(), "flash", "Snippet successfully created!")
+	app.sessionManager.Put(r.Context(), "flash", "Snippet successfully created!")
 
-    http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
 
 func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Display a form for signing up a new user")
+	data := app.newTemplateData(r)
+	data.Form = userSignupForm{}
+	app.render(w, r, http.StatusOK, "signup.tmpl", data)
 }
 
 func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Create a new user")
+	fmt.Fprintln(w, "Create a new user")
 }
 
 func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Display a form for logging in a user")
+	fmt.Fprintln(w, "Display a form for logging in a user")
 }
 
 func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Authenticate and login the user")  
+	fmt.Fprintln(w, "Authenticate and login the user")
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Logout the user")
+	fmt.Fprintln(w, "Logout the user")
 }
